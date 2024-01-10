5fa1a8885e2663a14594e3a39d01ef147053e6bc
LimChetra <limchetra60@gmail.com>
2024-01-10 19:48:08 +0700

    Convert Short Date to Full Date

---
+++ src/ShortDateConverter.java
@@ -0,0 +1,15 @@
+import java.text.DateFormatSymbols;
+import java.util.Scanner;
+public class ShortDateConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a short date (YYYY-MM-DD): ");
+        String shortDate = scanner.next();
+        int year = Integer.parseInt(shortDate.substring(0, 4));
+        int month = Integer.parseInt(shortDate.substring(5, 7));
+        int day = Integer.parseInt(shortDate.substring(8, 10));
+        String monthName = new DateFormatSymbols().getMonths()[month - 1];
+        String fullDate = monthName + " " + day + ", " + year;
+        System.out.println("Full Date: " + fullDate);
+    }
+}
